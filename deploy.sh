854b383aee8d5ea12fa0c593cb3c34fce0cc4d5c
alex stone <aharrystone@gmail.com>
2023-06-15 15:11:24 -0500

    updates

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+echo 'alexstone.io' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+
+
+git push -f git@github.com:alexhstone/alexstone-portfolio-site.git main:gh-pages
+
+cd -
